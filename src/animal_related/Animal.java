73636cb49985f4c5d1838ceeb615540275dd5c13
unknown <s24467@pjwstk.edu.pl>
2023-12-14 23:17:32 +0100

    filled in classes and functions: tbu, unit testing tba

--- src/animal_related/Animal.java
+++ src/animal_related/Animal.java
@@ -1,5 +1,84 @@
 package animal_related;
 
-public class Animal {
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
+public class Animal {
+	private int ID;
+    private String name;
+    private Date dateOfBirth;
+    private double weight;
+    private String species;
+    private String breed;
+    private Date foundDate;
+    private List<String> specialNeedsList;
+    private String status;
+
+
+    public Animal(int ID, String name, Date dateOfBirth, double weight, String species, String breed, Date foundDate, ArrayList<String> specialNeedsList) {
+        this.ID = ID;
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+        this.weight = weight;
+        this.species = species;
+        this.breed = breed;
+        this.foundDate = foundDate;
+        this.specialNeedsList = new ArrayList<>();
+        this.status = "At Shelter";
+    }
+    
+    public void viewAnimalDetails() {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+        StringBuilder details = new StringBuilder();
+        details.append("Animal ID: ").append(ID).append("\n");
+        details.append("Name: ").append(name).append("\n");
+        details.append("Date of Birth: ").append(dateFormat.format(dateOfBirth)).append("\n");
+        details.append("Weight: ").append(weight).append("\n");
+        details.append("Species: ").append(species).append("\n");
+        details.append("Breed: ").append(breed).append("\n");
+        details.append("Found Date: ").append(dateFormat.format(foundDate)).append("\n");
+        details.append("Special Needs: ").append(specialNeedsList).append("\n");
+        details.append("Status: ").append(status).append("\n");
+        details.append("------------------------").append("\n");
+
+        System.out.println(details.toString());
+    }
+
+    public static Animal addNewAnimal(int ID, String name, Date dateOfBirth, double weight, String species, String breed, Date foundDate, ArrayList<String> specialNeedsList) {
+        return new Animal(ID, name, dateOfBirth, weight, species, breed, foundDate, specialNeedsList);
+    }
+
+    public void editInformation(String name, Date dateOfBirth, Double weight, String species, String breed, Date foundDate, List<String> specialNeedsList, String status) {
+        if (name != null) {
+            this.name = name;
+        }
+        if (dateOfBirth != null) {
+            this.dateOfBirth = dateOfBirth;
+        }
+        if (weight != null) {
+            this.weight = weight;
+        }
+        if (species != null) {
+            this.species = species;
+        }
+        if (breed != null) {
+            this.breed = breed;
+        }
+        if (foundDate != null) {
+            this.foundDate = foundDate;
+        }
+        if (specialNeedsList != null) {
+            this.specialNeedsList = specialNeedsList;
+        }
+        if (status != null) {
+            this.status = status;
+        }
+    }
+
+    public int getID() {
+        return ID;
+    }
 }
--- src/animal_related/CareChecklist.java
+++ src/animal_related/CareChecklist.java
@@ -1,5 +1,27 @@
 package animal_related;
 
-public class CareChecklist {
+import java.util.List;
 
+public class CareChecklist {
+    private List<ChecklistItem> itemList;
+
+
+    public CareChecklist(List<ChecklistItem> itemList) {
+        this.itemList = itemList;
+    }
+    
+    public void addItem(ChecklistItem item) {
+        itemList.add(item);
+        System.out.println("Item added to the checklist: " + item.getName());
+    }
+
+    public void viewChecklist() {
+        System.out.println("Care Checklist:");
+        for (ChecklistItem item : itemList) {
+            System.out.println("Name: " + item.getName());
+            System.out.println("Description: " + item.getDescription());
+            System.out.println("Status: " + item.getStatus());
+            System.out.println("------------------------");
+        }
+    }
 }
